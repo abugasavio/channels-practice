c85e521bb8a79677c1d2e42fadbe28175d24197c
savioabuga <savioabuga@gmail.com>
2016-03-27 22:34:03 +0300

    updating models

--- posts/models.py
+++ posts/models.py
@@ -12,7 +12,7 @@ class Liveblog(models.Model):
         return self.title
         
     def get_absolute_url(self):
-        return "/liveblog/%s/" % self.blog
+        return "/liveblog/%s/" % self.slug
         
     @property
     def group_name(self):
@@ -36,3 +36,32 @@ class Post(models.Model):
 
     def html_body(self):
         return linebreaks_filter(self.body)
+
+    def send_notification(self):
+        """
+        Sends a notification to everyone in our Liveblog's group with our
+        content.
+        """
+        # Make the payload of the notification. We'll JSONify this, so it has
+        # to be simple types, which is why we handle the datetime here.
+        notification = {
+            "id": self.id,
+            "html": self.html_body(),
+            "created": self.created.strftime("%a %d %b %Y %H:%M"),
+        }
+        # Encode and send that message to the whole channels Group for our
+        # liveblog. Note how you can send to a channel or Group from any part
+        # of Django, not just inside a consumer.
+        Group(self.liveblog.group_name).send({
+            # WebSocket text frame, with JSON content
+            "text": json.dumps(notification),
+        })
+
+    def save(self, *args, **kwargs):
+        """
+        Hooking send_notification into the save of the object as I'm not
+        the biggest fan of signals.
+        """
+        result = super(Post, self).save(*args, **kwargs)
+        self.send_notification()
+        return result

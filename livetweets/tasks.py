3ab0521960536e51e69fe5873282bf6e85d6ee2b
savioabuga <savioabuga@gmail.com>
2016-03-28 10:40:53 +0300

    updating requirements

---
+++ livetweets/tasks.py
@@ -0,0 +1,23 @@
+from django.conf import settings
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+
+
+class StdOutListener(StreamListener):
+
+    def on_data(self, data):
+        print data
+        return True
+
+    def on_error(self, status):
+        print status
+
+
+def stream_tweets():
+    listener = StdOutListener()
+    auth = OAuthHandler(settings.TWITTER_CONSUMER_KEY, settings.TWITTER_CONSUMER_secret)
+    auth.set_access_token(settings.TWITTER_ACCESS_TOKEN, settings.TWITTER_ACCESS_TOKEN_SECRET)
+    stream = Stream(auth, listener)
+    # filter tweets
+    stream.filter(track=['python', 'javascript', 'ruby'])
